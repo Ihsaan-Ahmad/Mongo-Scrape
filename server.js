a6e3a08b013e3a59ac5b2d0a0ad71963b5e709b6
Ihsaan Ahmad <ihsaan.edu4321@gmail.com>
2019-11-08 23:38:30 -0600

    added clean files

---
+++ server.js
@@ -0,0 +1,34 @@
+// Set Handlebars.
+
+const express = require('express')
+const app = express()
+var mongoose = require("mongoose");
+const port = 3000
+var exphbs = require("express-handlebars");
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/articlesController.js");
+
+app.use(routes);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost:27017/cnn";
+
+mongoose.connect(db, {useNewUrlParser: true}, function(error) {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("mongoose connection is successful");
+    }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
